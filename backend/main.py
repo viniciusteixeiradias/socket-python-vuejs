8b40ab19e3699010df25cb7cba18d45e37d83eea
viniciusteixeiradias <viniciustdias2001@gmail.com>
2022-09-22 17:06:02 -0300

    add reconect and send objects

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 # uvicorn main:app --reload
+from ctypes import Union
+import string
 from typing import List
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+class Order(BaseModel):
+  id: int
+  name: str
+  description: str
+  price: float
+  tax: float
 
 app = FastAPI()
 
@@ -26,15 +34,51 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
+ordens = []
+
+
+async def notifications(qtd: string):
+  await manager.broadcast(qtd)
+
+
+@app.on_event("startup")
+async def startup_event():
+  order =  Order(
+    id = 1,
+    name = "Meu nome",
+    description = "Essa descrição é massa",
+    price = 3.9,
+    tax = 3
+  )
+  ordens.append(order)
+  print("Iniciei", ordens)
+
+    
+@app.post("/append/")
+async def create_order(order: Order):
+  ordens.append(order)
+  print("append", ordens)
+  await notifications(f"{len(ordens)}")
+
+@app.delete("/remove/{id}")
+async def del_order(id: int):
+
+  for index, ordem in enumerate(ordens):
+    if ordem.id == id:
+      del(ordens[index])
+
+  print("del", ordens)
+
+  await notifications(f"{len(ordens)}")
+
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
-  await manager.connect(websocket)
   try:
+    await manager.connect(websocket)
     while True:
-      data = await websocket.receive_text()
-      await manager.send_personal_message(f"You wrote: {data}", websocket)
-      await manager.broadcast(f"Client #{client_id} says: {data}")
+      await manager.broadcast(f"{len(ordens)}")
+      await websocket.receive_text()
   except WebSocketDisconnect:
     manager.disconnect(websocket)
-    await manager.broadcast(f"Client #{client_id} left the chat")
+    # await manager.broadcast(f"Client #{client_id} left the chat")
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,61 +1,45 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-
-interface Notification {
-  author: string
-  message: string
-}
-
 interface State {
-  notifications: Notification[]
-  messages: string[]
-  text: string
+  n1: number
 }
 
 const count = ref(0)
 const client_id = Date.now()
-const connection = new WebSocket(`ws://localhost:8000/ws/${client_id}`)
 const state = reactive<State>({
-  notifications: [],
-  messages: [],
-  text: ""
+  n1: 0
 })
 
-connection.onmessage = function (event) {
-  // const newItem = { ...JSON.parse(event.data) }
-  // state.notifications.push(newItem)
-  state.messages.push(event.data as string)
-  showNotification()
-}
+connect()
 
-const submit = () =>
-  connection.send(state.text)
-  state.text = ""
-    // JSON.stringify({
-    //   author: "Vinícius",
-    //   message: "As cotações foram atualizadas"
-    // })
-  
-const showNotification = () => {
-  console.log("You have new notifications")
+function connect() {
+  var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`)
+  ws.onmessage = function (event) {
+    state.n1 = event.data as number
+    showNotification()
+  }
+
+  ws.onclose = function(e) {
+    console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+    setTimeout(function() {
+      connect();
+    }, 5000);
+  };
+
+  ws.onerror = function(err) {
+    console.error('Socket encountered error: ', err, 'Closing socket');
+    ws.close();
+  };
+
+  const showNotification = () => {
+    console.log("You have new notifications")
+  }
 }
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <input v-model="state.text" type="text" id="messageText" autocomplete="off"/>
-    <button type="button" @click="submit()">Submit</button>
-    <ul>
-      <li v-for="(message, index) in state.messages" :key="index">
-        {{message}}
-      </li>
-    </ul>
-  </div>
+  <h1>You have {{state.n1}} notifications</h1>
 </template>
 
 <style scoped>
